d4f6df3d3dc26c0ab0e5dffa2871875e5413087c
Papy Poc <stephane.morin4@orange.fr>
2024-03-07 21:19:04 +0100

    ds

--- glpi-install.sh
+++ glpi-install.sh
@@ -37,7 +37,7 @@ VERSION=$(lsb_release -rs 2>/dev/null)
 # Vérifie si c'est une distribution Debian
 if [ "$DISTRO" == "Debian" ] || [ "$DISTRO" == "Ubuntu" ]; then
         # Vérifie si la version de Debian est acceptable
-        if [[ " ${DEBIAN_VERSIONS[*]} " == *" $VERSION "* ] || [ " ${UBUNTU_VERSIONS[*]} " == *" $VERSION "* ]]; then
+        if [[ " ${DEBIAN_VERSIONS[*]} " == *" $VERSION "* ]] || [[ " ${UBUNTU_VERSIONS[*]} " == *" $VERSION "* ]]; then
                 info "La version de votre système d'exploitation ($DISTRO $VERSION) est compatible."
         else
                 warn "La version de votre système d'exploitation ($DISTRO $VERSION) n'est pas considérée comme compatible."
