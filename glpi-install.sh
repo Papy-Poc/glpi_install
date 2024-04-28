1efffed45aaa3b79818d8e0ff88acde4d2d6e5ec
Papy Poc <stephane.morin4@orange.fr>
2024-04-28 10:05:56 +0200

    update

--- glpi-install.sh
+++ glpi-install.sh
@@ -65,7 +65,7 @@ function check_install(){
                 sleep 2
                 glpi_cli_version=$(sed -n 's/.*GLPI CLI \([^ ]*\).*/\1/p' <<< "$output")
                 warn "Le site est déjà installé. Version ""$glpi_cli_version"
-                new_version=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | jq -r '.name')
+                new_version=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | jq -r '.assets[0].browser_download_url')
                 info "Nouvelle version trouver : GLPI version $new_version"
                 info "Voulez-vous mettre à jour GLPI (O/N): "
                 read -r MaJ
