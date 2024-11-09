55ac765740452ae0c1df50c927469851b3599762
Papy Poc <stephane.morin63@free.fr>
2024-11-09 20:49:16 +0100

    Update test-distri.sh

--- test-distri.sh
+++ test-distri.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-if [ -f /etc/debian_version ]; then
-    echo "Système Debian"
-elif [ -f /etc/redhat-release ]; then
-    echo "Système Red Hat"
+# Nom de la distribution
+distri=$(grep -oP '(?<=ID=").*(?=")' /etc/os-release | sed -n '1p')
+# Version
+version=$(grep -oP '(?<=VERSION=").*(?=")' /etc/os-release | sed -n '2p')
+
+if [[ "$distri" == "ubuntu" && "$version" == "22.04" ]]; then
+    echo "Distribution : $distri $version"
+elif [[ "$distri" == "debian" && "$version" =~ ^11\..* ]]; then
+    echo "Distribution : $distri $version"
+elif [[ "$distri" == "almalinux" && "$version" =~ ^9\..* ]]; then
+    echo "Distribution : $distri $version"
+elif [[ "$distri" == "centos" && "$version" =~ ^8\..* ]]; then
+    echo "Distribution : $distri $version"
 else
-    echo "Système inconnu"
+  echo "Distribution non prise en charge : $distri_name $version"
 fi
