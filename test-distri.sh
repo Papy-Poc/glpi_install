9a5cd822463f87cc66fe7510074bee66f4e9cd80
Papy Poc <stephane.morin63@free.fr>
2024-11-09 21:46:17 +0100

    Update test-distri.sh

--- test-distri.sh
+++ test-distri.sh
@@ -5,14 +5,23 @@ distri=$(grep -oP '(?<=ID=").*(?=")' /etc/os-release | sed -n '1p')
 # Version
 version=$(grep -oP '(?<=VERSION=").*(?=")' /etc/os-release | sed -n '2p')
 
-if [[ "$distri" == "ubuntu" && "$version" == "22.04" ]]; then
+if [[ "$distri" == "ubuntu" && "$version" == "23.10" ]]; then
+    echo "Distribution : $distri $version"
+elif [[ "$distri" == "ubuntu" && "$version" == "24.10" ]]; then
     echo "Distribution : $distri $version"
 elif [[ "$distri" == "debian" && "$version" =~ ^11\..* ]]; then
     echo "Distribution : $distri $version"
+elif [[ "$distri" == "debian" && "$version" =~ ^12\..* ]]; then
+    echo "Distribution : $distri $version"
 elif [[ "$distri" == "almalinux" && "$version" =~ ^9\..* ]]; then
     echo "Distribution : $distri $version"
-elif [[ "$distri" == "centos" && "$version" =~ ^8\..* ]]; then
+elif [[ "$distri" == "centos" && "$version" =~ ^9\..* ]]; then
+    echo "Distribution : $distri $version"
+elif [[ "$distri" == "rocky linux" && "$version" =~ ^9\..* ]]; then
     echo "Distribution : $distri $version"
 else
   echo "Distribution non prise en charge : $distri_name $version"
 fi
+
+
+24.10
